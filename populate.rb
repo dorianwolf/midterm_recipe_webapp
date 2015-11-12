7af7e05ef276fe2765b229a7e499254c7a37f3bc
vagrant <vagrant@lighthouse-labs>
2015-11-12 17:05:15 +0000

    Populate changed

--- populate.rb
+++ populate.rb
@@ -12,9 +12,35 @@ require './app/models/recipe'
 require './config/environment'
 require './config/database'
 
-9.times do
+inventory_carbs = ["bread", "rice", "wheat", "flour"]
+
+inventory_vegetables = ["potatos", "onions", "garlic", "ginger", "lettuce", "kale", "mushrooms", "corn"]
+
+inventory_fruits = ["mangoes", "oranges", "grapes", "pineapples", "bananas", "apples"]
+
+inventory_proteins = ["beef", "pork", "chicken", "turkey", "eggs", "milk", "beans"]
+
+inventory_carbs.each do |carb|
   Inventory.create!(
-    name: Faker::Name.name
+    name: carb
+  )
+end
+
+inventory_vegetables.each do |veggies|
+  Inventory.create!(
+    name: veggies
+  )
+end
+
+inventory_fruits.each do |fruit|
+  Inventory.create!(
+    name: fruit
+  )
+end
+
+inventory_proteins.each do |protein|
+  Inventory.create!(
+    name: protein
   )
 end
 
